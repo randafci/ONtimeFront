0376af4bf6d304390b69b0bf3597b42a904f0fe4
Embaby97 <embaby97.ae@gmail.com>
2025-08-24 18:10:07 +0300

    Add new permission service and environment files

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AppConfigService } from '@/pages/service/app-config.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+export function initializeApp(appConfig: AppConfigService) {
+  return () => appConfig.loadConfig();
+}
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterModule],
+     imports: [RouterModule,CommonModule, HttpClientModule],
+  providers: [
+    AppConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeApp,
+      deps: [AppConfigService],
+      multi: true
+    }
+  ],
     template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {}
